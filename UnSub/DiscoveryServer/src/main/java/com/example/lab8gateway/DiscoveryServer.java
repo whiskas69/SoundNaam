6e65b241d4cd4c0474689c384f83ce10e4001168
whiskas69 <88488048+whiskas69@users.noreply.github.com>
2024-01-16 22:57:08 +0700

    Add files via upload

---
+++ UnSub/DiscoveryServer/src/main/java/com/example/lab8gateway/DiscoveryServer.java
@@ -0,0 +1,15 @@
+package com.example.lab8gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class DiscoveryServer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryServer.class, args);
+	}
+
+}
---
+++ UnSub/DiscoveryServer/src/test/java/com/example/lab8gateway/ServiceDiscoveryTests.java
@@ -0,0 +1,13 @@
+package com.example.lab8gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
